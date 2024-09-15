f2351a783d3cf4f9f542cf5000b0bd2ca0dba22b
Joel Lorenz Boada <joel.lorenz2000@gmail.com>
2024-09-15 17:18:53 +0200

    Update encryption key in app configuration

--- config/app.php
+++ config/app.php
@@ -97,7 +97,7 @@ return [
 
     'cipher' => 'AES-256-CBC',
 
-    'key' => env('APP_KEY'),
+    'key' => 'base64:K6TJK0z9+eNu1zQqOQL8zerdYzVv/JzF6z96kdWGEkI=',
 
     'previous_keys' => [
         ...array_filter(
